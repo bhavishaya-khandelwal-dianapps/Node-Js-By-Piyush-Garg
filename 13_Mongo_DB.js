600014679f40ea4269c1918200a7f5e121b1d45e
bhavishaya-khandelwal-dianapps <bhavishaya.khandelwal@dianapps.com>
2025-01-03 10:44:11 +0530

    Setting up Mongo DB

---
+++ 13_Mongo_DB.js
@@ -0,0 +1,7 @@
+//* Mongo DB -> Mongo DB is a Database  
+//? No-SQL Document Based DataBase 
+//? Strong Support for Aggregation Pipes 
+//? Works on BSON format 
+//? Best for Node Application
+
+//? In SQL, we have tables, but in Mongo DB we have collections 
